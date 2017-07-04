ea70105cbe145858f09c251893dde67bdfd45750
Sam Kheirandish <sam.kheirandish@fortify.pro>
2017-07-04 15:15:55 -0700

    refactoring

---
+++ completed/MapApp/ViewController.swift
@@ -0,0 +1,82 @@
+//
+//  ViewController.swift
+//  MapApp
+//
+//  Created by Sam on 2017-06-17.
+//  Copyright © 2017 Sam. All rights reserved.
+//
+
+import UIKit
+import MapKit
+import CoreLocation
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var map: MKMapView!
+    let locationManager = CLLocationManager()
+    var isCentered = false
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+        
+    }
+    
+    func centerMapOnLocation(coordinate: CLLocationCoordinate2D) {
+        let latDelta:CLLocationDegrees = 0.01
+        let lonDelta:CLLocationDegrees = 0.01
+        let span:MKCoordinateSpan = MKCoordinateSpanMake(latDelta, lonDelta)
+        let region:MKCoordinateRegion = MKCoordinateRegionMake(coordinate, span)
+        self.map.setRegion(region, animated: true)
+    }
+    
+    @IBAction func mapTypeChanged(sender: UISegmentedControl) {
+        switch (sender.selectedSegmentIndex) {
+        case 0:
+            map.mapType = MKMapType.standard
+        case 1:
+            map.mapType = MKMapType.hybrid
+        case 2:
+            map.mapType = MKMapType.satellite
+        default: break
+        }
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
+
+
+extension ViewController: CLLocationManagerDelegate {
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        if !isCentered {
+            self.centerMapOnLocation(coordinate: locations.first!.coordinate)
+            isCentered = true
+        }
+        
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        
+        switch status {
+        case .authorizedAlways, .authorizedWhenInUse:
+            locationManager.startUpdatingLocation()
+            map.showsUserLocation = true
+        default: 
+            locationManager.stopUpdatingLocation()
+            map.showsUserLocation = false
+        }
+    }
+}
+
